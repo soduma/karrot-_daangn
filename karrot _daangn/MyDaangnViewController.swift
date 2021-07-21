88d2f0ff110f1a8fbd078e9d03a58f449af6cd21
soduma <soduma@naver.com>
2021-07-21 17:31:50 +0900

    scrollview

---
+++ karrot _daangn/MyDaangnViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MyDaangnViewController.swift
+//  karrot _daangn
+//
+//  Created by 장기화 on 2021/07/21.
+//
+
+import UIKit
+
+class MyDaangnViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        let label = UILabel()
+//        label.textColor = .black
+//        label.text = "나의 당근"
+//        self.navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: label)
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
